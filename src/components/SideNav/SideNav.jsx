77d9c41b8478b6121454c37f181368f392b8a794
hpteach2021 <hooper.rob@gmail.com>
2021-10-11 16:59:47 +0100

    lined up nav items nicely

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
 import "./SideNav.scss";
+import home from "./homeToDelete.svg"
+import logo from "./logoToDelete.svg"
 
 const SideNav = () => {
   return (
     <div className="sidenav">
 
       <div className="sidenav__logo">
-        
+        <img src={logo} alt="" />
       </div>
 
-      <div className="sidenav__menu--items">
-        <div className="sidenav__dashboard">
+      <div className="sidenav__items">
+        <div className="sidenav__dashboard sidenav__item">
+          <img className="sidenav__item--image"src={home} alt="" />
           <p>Dashboard</p>
         </div>
 
-        <div className="sidenav__security">
+        <div className="sidenav__security sidenav__item">
+          <img className="sidenav__item--image"src={home} alt="" />
           <p>Security</p>
         </div>
 
-        <div className="sidenav__devices">
+        <div className="sidenav__devices sidenav__item">
+          <img className="sidenav__item--image"src={home} alt="" />
           <p>Device</p>
         </div>
 
-        <div className="sidenav__network">
+        <div className="sidenav__network sidenav__item">
+          <img className="sidenav__item--image"src={home} alt="" />
           <p>Network</p>
         </div>
 
-        <div className="sidenav__settings">
+        <div className="sidenav__settings sidenav__item">
+          <img className="sidenav__item--image"src={home} alt="" />
           <p>Settings</p>
         </div>
       </div>
+
+      <div></div>
 
       <div className="sidenav__logout">
         <p>Logout</p>
