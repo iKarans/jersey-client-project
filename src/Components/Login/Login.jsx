cfc16376a855f9deb3da64ebfdd514f96f72ac09
gabagardi <g.agardi88@gmail.com>
2021-10-12 16:25:46 +0100

    form finished, adding bottom items

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -11,6 +11,20 @@ const Login = () => {
             <LoginPageNav />
             <div className="login__page">
                 <img src={Logo} alt="lujam logo" className="login__page--logo"/>
+                <form action="submit" className="login__page--form">
+                    <div className="login__page--form-rectangle"></div>
+                    <h3 className="login__page--form-header">Login</h3>
+                    <label htmlFor="email" className="login__page--form-label">Email Address</label>
+                    <input type="text" id="email" className="login__page--form-input"/>
+                    <label htmlFor="email" className="login__page--form-label">Name</label>
+                    <input type="password" id="name" className="login__page--form-input"/>
+                    <input type="checkbox" id="remember-me" className="login__page--form-checkbox" />
+                    <label htmlFor="remember-me" className="checkbox-label">Remember Me</label>
+                    <button className="login__page--form-button">Login</button>
+                </form>
+                <p className="login__page--sign-up">Dont have an account? <a classname="login__page--link "href="#">Sign Up</a></p>
+                <a classname="login__page--link "href="#">Forgotten Your Password?</a>
+                
             </div>
             
         </section>
