fd758ed4345dc53ed905041d1ad50396508e43fa
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-12 16:55:48 +0100

    high risk button

--- src/components/SecurityBar/HighRisk/HighRisk.jsx
+++ src/components/SecurityBar/HighRisk/HighRisk.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import "./HighRisk.scss";
+import highRiskWhite from "../../../assets/header/high-risk-white.svg";
 
 const HighRisk = () => {
   return (
-    <div>
-      
+    <div className="high-risk">
+      <div className="high-risk__icon">
+        <img className="high-risk__icon--svg" src={highRiskWhite} alt="high risk" />
+      </div>
+      <div className="high-risk__info">
+        <p className="high-risk__info--heading">
+          High Risk Devices
+        </p>
+        <p className="high-risk__info--number">
+          4
+        </p>
+      </div>  
     </div>
   )
 }
