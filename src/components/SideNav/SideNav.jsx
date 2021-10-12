68537c29fc6a0b7d6f8272292cb68a2bd8ef22a0
iagardi <garbigz@gmail.com>
2021-10-12 16:17:32 +0100

    devices svg icon changes color on hover

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -6,6 +6,7 @@ import device from "../../assets/nav/device.svg";
 import network from "../../assets/nav/network.svg";
 import settings from "../../assets/nav/settings.svg";
 import logo from "../../assets/global/lujam-logo-green.svg"
+import { ReactComponent as Logo } from "../../assets/nav/device.svg"
 
 
 const SideNav = () => {
@@ -18,27 +19,27 @@ const SideNav = () => {
 
       <div className="sidenav__items">
         <div className="sidenav__dashboard sidenav__item">
-          <img className="sidenav__item--image"src={home} alt="" />
+          <img className="sidenav__item--image" src={home} alt="" />
           <p>Dashboard</p>
         </div>
 
         <div className="sidenav__security sidenav__item">
-          <img className="sidenav__item--image"src={security} alt="" />
+          <img className="sidenav__item--image" src={security} alt="" />
           <p>Security</p>
         </div>
 
         <div className="sidenav__devices sidenav__item">
-          <img className="sidenav__item--image"src={device} alt="" />
+          <Logo className="sidenav__item--image" />
           <p>Device</p>
         </div>
 
         <div className="sidenav__network sidenav__item">
-          <img className="sidenav__item--image"src={network} alt="" />
+          <img className="sidenav__item--image" src={network} alt="" />
           <p>Network</p>
         </div>
 
         <div className="sidenav__settings sidenav__item">
-          <img className="sidenav__item--image"src={settings} alt="" />
+          <img className="sidenav__item--image" src={settings} alt="" />
           <p>Settings</p>
         </div>
       </div>
