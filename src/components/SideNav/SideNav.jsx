d806fe02f9e786fc5bbf4c1f19864fec9548e5b4
hpteach2021 <hooper.rob@gmail.com>
2021-10-12 16:19:56 +0100

    adding changes

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -10,44 +10,48 @@ import logo from "../../assets/global/lujam-logo-green.svg"
 
 const SideNav = () => {
   return (
-    <div className="sidenav">
+    <div className="navMenus">
+      <div className="sidenav">
 
-      <div className="sidenav__logo">
-        <img src={logo} alt="" />
+        <div className="sidenav__logo">
+          <img src={logo} alt="" />
+        </div>
+
+        <div className="sidenav__items">
+          <div className="sidenav__dashboard sidenav__item">
+            <img className="sidenav__item--image"src={home} alt="" />
+            <p>Dashboard</p>
+          </div>
+
+          <div className="sidenav__security sidenav__item">
+            <img className="sidenav__item--image"src={security} alt="" />
+            <p>Security</p>
+          </div>
+
+          <div className="sidenav__devices sidenav__item">
+            <img className="sidenav__item--image"src={device} alt="" />
+            <p>Device</p>
+          </div>
+
+          <div className="sidenav__network sidenav__item">
+            <img className="sidenav__item--image"src={network} alt="" />
+            <p>Network</p>
+          </div>
+
+          <div className="sidenav__settings sidenav__item">
+            <img className="sidenav__item--image"src={settings} alt="" />
+            <p>Settings</p>
+          </div>
+        </div>
+
+        <div className="sidenav__logout">
+          <p>Logout</p>
+        </div>
+
       </div>
-
-      <div className="sidenav__items">
-        <div className="sidenav__dashboard sidenav__item">
-          <img className="sidenav__item--image"src={home} alt="" />
-          <p>Dashboard</p>
-        </div>
-
-        <div className="sidenav__security sidenav__item">
-          <img className="sidenav__item--image"src={security} alt="" />
-          <p>Security</p>
-        </div>
-
-        <div className="sidenav__devices sidenav__item">
-          <img className="sidenav__item--image"src={device} alt="" />
-          <p>Device</p>
-        </div>
-
-        <div className="sidenav__network sidenav__item">
-          <img className="sidenav__item--image"src={network} alt="" />
-          <p>Network</p>
-        </div>
-
-        <div className="sidenav__settings sidenav__item">
-          <img className="sidenav__item--image"src={settings} alt="" />
-          <p>Settings</p>
-        </div>
-      </div>
-
-      <div className="sidenav__logout">
-        <p>Logout</p>
-      </div>
-
     </div>
+    
+    
   );
 };
 
