963290fd2a4fec2728448d56f8edc64731c06061
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-12 16:42:43 +0100

    alert button component elements added

--- src/components/SecurityBar/Alerts/Alerts.jsx
+++ src/components/SecurityBar/Alerts/Alerts.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import alertIcon from "../../../assets/header/alert-icon.svg"
 
 const Alerts = () => {
   return (
-    <div>
+    <div className="alerts">
+      <div className="alerts__icon">
+        <img className="alerts__icon--svg" src={alertIcon} alt="alerts" />
+      </div>
+      <div className="alerts__info">
+        <p className="alerts__info--heading">
+          Alerts
+        </p>
+        <p className="alerts__info--number">
+          63
+        </p>
+      </div>
       
     </div>
   )
