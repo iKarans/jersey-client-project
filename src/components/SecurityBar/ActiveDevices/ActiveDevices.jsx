f93aab7d68b89cdbd53d6a47596db71757e16043
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-12 15:59:44 +0100

    active devices component elements added

--- src/components/SecurityBar/ActiveDevices/ActiveDevices.jsx
+++ src/components/SecurityBar/ActiveDevices/ActiveDevices.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import deviceIcon from "../../../assets/header/device-icon.svg"
 
 const ActiveDevices = () => {
   return (
-    <div>
+    <div className="active-devices">
+      <div className="active-devices__icon">
+        <img src={deviceIcon} alt="active devices" />
+      </div>
+      <div className="active-devices__info">
+        <p className="active-devices__info--heading">
+          Active Devices
+        </p>
+        <p className="active-devices__info--number">
+          36
+        </p>
+      </div>
       
     </div>
   )
