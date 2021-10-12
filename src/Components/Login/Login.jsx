3798b4916c59729e8cd96d1f03342ead7127bada
gabagardi <g.agardi88@gmail.com>
2021-10-12 15:43:13 +0100

    login nav finished, rendered, lujam logo on login page added, bem updated

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import LoginPageNav from '../LoginPageNav/LoginPageNav'
+import Logo from "../../assets/global/lujam-logo-white.svg"
+
+import "./Login.scss"
 
 const Login = () => {
     return (
-        <section className="login__page">
-
+        <section className="login">
+            <LoginPageNav />
+            <div className="login__page">
+                <img src={Logo} alt="lujam logo" className="login__page--logo"/>
+            </div>
+            
         </section>
     )
 }
--- src/Components/LoginPageNav/LoginPageNav.jsx
+++ src/Components/LoginPageNav/LoginPageNav.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
+import Logo from "../../assets/global/lujam-logo-white.svg"
+
+import "./LoginPageNav.scss"
+
 const LoginPageNav = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className="login__nav">
+            <img src={Logo} alt="Lujam logo" className="login__nav--logo"/>
+            <ul className="login__nav--links">
+                <li className="login__nav-link">About</li>
+                <li className="login__nav-link">How it works?</li>
+                <li className="login__nav-link">Contact</li>
+            </ul>
+        </nav>
     )
 }
 
