f6000e3f312bf87a1a17fa12028827e8fe18a521
itsyas <itsyas@users.noreply.github.com>
2021-10-12 14:27:21 +0100

    old changes to branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import logo from './logo.svg';
 import './App.css';
 import SideNav from './components/SideNav/SideNav';
@@ -8,12 +8,11 @@ const App = () => {
     <SideNav />
   );
 }
-=======
+
 import "./App.css";
 
 const App = () => {
   return <div className="App"></div>;
 };
->>>>>>> a1dfc5266cfd9017810f4dc7f1ebfb5853b5e1f7
 
 export default App;
