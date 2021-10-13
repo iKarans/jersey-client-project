16f14aecad0b7bdd7ae0d75173af239bc9c62bf6
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-10-13 09:52:06 +0100

    added search bar

--- src/containers/DeviceList/DeviceList.jsx
+++ src/containers/DeviceList/DeviceList.jsx
@@ -3,14 +3,32 @@ import { devices } from "../../data/devices";
 import { DeviceBar } from "../../components/DeviceBar/DeviceBar";
 import "./DeviceList.scss";
 console.log(devices);
+const handleinput = () => {};
 export const DeviceList = () => {
   return (
-    <div className="deviceList">
-      {devices && devices.length !== 0 ? (
-        devices.map((device) => <DeviceBar key={device.name} device={device} />)
-      ) : (
-        <p>no devices found</p>
-      )}
+    <div className="devices">
+      <div classname="searchOptions">
+        <form className="search-bar">
+          <label htmlFor="deviceSearch" className="search-bar__label">
+            Search
+          </label>
+          <input
+            type="text"
+            name="deviceSearch"
+            onInput={handleinput()}
+            className="search-bar__input"
+          />
+        </form>
+      </div>
+      <div className="devicesList">
+        {devices && devices.length !== 0 ? (
+          devices.map((device) => (
+            <DeviceBar key={device.name} device={device} />
+          ))
+        ) : (
+          <p>no devices found</p>
+        )}
+      </div>
     </div>
   );
 };
