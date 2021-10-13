14d189159f259dbfaf4bdbca6d3b93f1b177b77a
iagardi <garbigz@gmail.com>
2021-10-13 14:54:16 +0100

    updated chat icon

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -13,14 +13,14 @@ const Login = () => {
         <section className="login">
             <LoginPageNav />
             <div className="login__page">
-                <img src={Logo} alt="lujam logo" className="login__page--logo"/>
+                <img src={Logo} alt="lujam logo" className="login__page--logo" />
                 <form action="submit" className="login__page--form">
                     <div className="login__page--form-rectangle"></div>
                     <h3 className="login__page--form-header">Login</h3>
                     <label htmlFor="email" className="login__page--form-label">Email Address</label>
-                    <input type="text" id="email" className="login__page--form-input"/>
+                    <input type="text" id="email" className="login__page--form-input" />
                     <label htmlFor="email" className="login__page--form-label">Password</label>
-                    <input type="password" id="password" className="login__page--form-input"/>
+                    <input type="password" id="password" className="login__page--form-input" />
                     <input type="checkbox" id="remember-me" className="login__page--form-checkbox" />
                     <label htmlFor="remember-me" className="checkbox-label">Remember Me</label>
                     <button className="login__page--form-button">Login</button>
@@ -28,7 +28,9 @@ const Login = () => {
                 <p className="login__page--sign-up">Don't have an account? <a href="#">Sign Up</a></p>
                 <a className="login__page--link" href="#">Forgotten Your Password?</a>
             </div>
-            <img src={ChatIcon} alt="chat icon" className="login__chat-button" />
+            <div className="login__chat">
+                <img src={ChatIcon} alt="chat icon" className="login__chat-button" />
+            </div>
         </section>
     )
 }
