de10a4107c9d1a30bd20ee06c7a73255d6604601
aliabdellatif0 <ali.abdlaitf@gmail.com>
2021-10-14 16:11:06 +0100

    created the login form component which was lifted out from login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -4,41 +4,20 @@ import LoginPageNav from '../LoginPageNav/LoginPageNav'
 
 import Logo from "../../assets/global/lujam-logo-white.svg"
 import ChatIcon from "../../assets/login/chat-icon.svg"
-import ShowPassword from "../../assets/login/show-password.svg"
-import HidePassword from "../../assets/login/Group.svg"
+
+import LoginForm from '../LoginForm/LoginForm'
 
 import "./Login.scss"
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState(false)
-    const handleShowPassword = () => {
-        setShowPassword(!showPassword)
-    }
-
-
+    
     return (
         <section className="login">
             <LoginPageNav />
             <div className="login__page">
                 <img src={Logo} alt="lujam logo" className="login__page--logo" />
-                <form action="submit" className="login__page--form">
-                    <div className="login__page--form-rectangle"></div>
-                    <h3 className="login__page--form-header">Login</h3>
-                    <label htmlFor="email" className="login__page--form-label">Email Address</label>
-                    <input type="text" id="email" className="login__page--form-input" />
-                    <label htmlFor="password" className="login__page--form-label">Password</label>
-                    <div className="login__page--form-password-container">
-                        <input type={!showPassword ? "password" : "text"} id="password" className="login__page--form-input" />
-                        {!showPassword && <img src={HidePassword} className="hidepassword" alt="Hide password icon" onClick={handleShowPassword} />}
-                        {showPassword && <img src={ShowPassword} className="showpassword" alt="Show password icon" onClick={handleShowPassword} />}
-                    </div>
-                    <div className="login__page--form-checkbox-container"><input type="checkbox" id="remember-me" className="login__page--form-checkbox" />
-                        <label htmlFor="remember-me" className="checkbox-label">Remember Me</label>
-                    </div>
-                    <button className="login__page--form-button">Login</button>
-                </form>
-                <p className="login__page--sign-up">Don't have an account? <span href="#">Sign Up</span></p>
-                <p className="login__page--link" href="#">Forgotten Your Password?</p>
+                <LoginForm/>
+                
             </div>
             <div className="login__chat">
                 <img src={ChatIcon} alt="chat icon" className="login__chat-button" />
---
+++ src/Components/LoginForm/LoginForm.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react'
+
+import ShowPassword from "../../assets/login/show-password.svg"
+import HidePassword from "../../assets/login/Group.svg"
+
+import "./LoginForm.scss"
+
+const LoginForm = () => {
+    
+    const [showPassword, setShowPassword] = useState(false)
+    const handleShowPassword = () => {
+        setShowPassword(!showPassword)
+    }
+    
+    return (
+        <div>
+            <form action="submit" className="login__form">
+                <div className="login__form-rectangle"></div>
+                    <h3 className="login__form-header">Login</h3>
+                    <label htmlFor="email" className="login__form-label">Email Address</label>
+                    <input type="text" id="email" className="login__form-input" />
+                    <label htmlFor="password" className="login__form-label">Password</label>
+                    <div className="login__form-password-container">
+                        <input type={!showPassword ? "password" : "text"} id="password" className="login__form-input" />
+                        {!showPassword && <img src={HidePassword} className="hidepassword" alt="Hide password icon" onClick={handleShowPassword} />}
+                        {showPassword && <img src={ShowPassword} className="showpassword" alt="Show password icon" onClick={handleShowPassword} />}
+                    </div>
+                    <div className="login__form-checkbox-container"><input type="checkbox" id="remember-me" className="login__form-checkbox" />
+                        <label htmlFor="remember-me" className="checkbox-label">Remember Me</label>
+                    </div>
+                    <button className="login__form-button">Login</button>
+            </form>
+                
+            <p className="login__sign-up">Don't have an account? <span>Sign Up</span></p>
+            <p className="login__link">Forgotten Your Password?</p>
+            
+        </div>
+    )
+}
+
+export default LoginForm
+
