4e531974a0873876a65937377221fcce7c6df097
hpteach2021 <hooper.rob@gmail.com>
2021-10-15 14:07:50 +0100

    for adam

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -9,13 +9,10 @@ import Settings from '../../components/Routes/Routes.jsx'
 
 const Routes = () => {
    
-<<<<<<< HEAD
-=======
    const handleClick = () => {
       console.log("working")
     }
   
->>>>>>> 369b3e4634cb8b04f4e0e02fbd48a481be60ddff
 
 
    return (
@@ -26,21 +23,6 @@ const Routes = () => {
                <Dashboard />
             </Route>
             <Route exact path="/devices">
-<<<<<<< HEAD
-               <SideNav selectedPage= "Devices" />
-               <Devices />
-            </Route>
-            <Route exact path="/security">
-               <SideNav selectedPage= "Security" />
-               <Security />
-            </Route>
-            <Route exact path="/network">
-               <SideNav selectedPage= "Network" />
-               <Network />
-            </Route>
-            <SideNav selectedPage= "Settings" />
-            <Route exact path="/settings">
-=======
                <SideNav selectedPage= "Devices"  />
                <Devices />
             </Route>
@@ -52,9 +34,8 @@ const Routes = () => {
                <SideNav selectedPage= "Network"  />
                <Network />
             </Route>
-            <Route exact path="/settings">
                <SideNav selectedPage= "Settings"  />
->>>>>>> 369b3e4634cb8b04f4e0e02fbd48a481be60ddff
+               <Route exact path="/settings">
                <Settings />
             </Route>
          </Switch>
