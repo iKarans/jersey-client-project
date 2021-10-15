7558ad7888a4eaee684eecf07968c06ccf2260c1
hpteach2021 <hooper.rob@gmail.com>
2021-10-15 13:43:58 +0100

    forgot to change top bit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
 
 import './App.css';
-<<<<<<< HEAD
-import SideNav from './components/Nav/SideNav/SideNav';
-import Dashboard from './components/Dashboard/Dashboard';
-import Devices from './components/Devices/Devices';
-import Security from './components/Security/Security';
-import Network from './components/Network/Network';
-import Settings from './components/Settings/Settings';
-import Header from "./containers/Header/Header";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-=======
 import { BrowserRouter as Router } from "react-router-dom";
->>>>>>> e8af90fce13edb048a9668d0eb0f1c01615b96ed
 import Routes from './components/Routes/Routes';
 import Header from "./containers/Header/Header";
 
