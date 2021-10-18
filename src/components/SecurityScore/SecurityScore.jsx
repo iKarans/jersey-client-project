f1910d841f8bac20ec0b11ce32e16ad361863fcf
AdamDCosta <adam.dcosta@googlemail.com>
2021-10-18 16:19:20 +0100

    security score component folder added

---
+++ src/components/SecurityScore/SecurityScore.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SecurityScore = () => {
+
+  
+
+
+  return (
+    
+    <article className="security-score">
+      <div>
+        
+      </div>
+
+    </article>
+  )
+}
+
+export default SecurityScore
