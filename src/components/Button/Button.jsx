f73f0becffee5774a1503654b87725bb66d8335d
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-10-18 15:16:30 +0100

    resolving merge conflict

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import "./Button.scss";
-
-const Button = () => {
-  return <div className="button">Button works</div>;
-};
-
-export default Button;
--- src/components/Card/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import "./Card.scss";
-
-const Card = () => {
-  return <div className="card">Card works</div>;
-};
-
-export default Card;
