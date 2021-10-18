8a0683b2adaebc10e0c20c32e16a6a1419b38820
gabagardi <g.agardi88@gmail.com>
2021-10-18 16:27:12 +0100

    networkitem jsx created, rendered network data in network

--- src/components/NetworkItem/NetworkItem.jsx
+++ src/components/NetworkItem/NetworkItem.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const NetworkItem = () => {
-  return <div></div>;
+const NetworkItem = (props) => {
+  const { name, summary, created, importance } = props;
+
+  return (
+    <div className="network-item">
+      <h5 className="network-item__name">{name}</h5>
+      <p className="network-item__parapgraph summary">{summary}</p>
+      <p className="network-item__parapgraph created">{created}</p>
+      <h5 className="network-item__importance">{importance}</h5>
+      <div className="network-item__option">
+        <div className="network-item__option-dot"></div>
+        <div className="network-item__option-dot"></div>
+        <div className="network-item__option-dot"></div>
+      </div>
+    </div>
+  );
 };
 
 export default NetworkItem;
