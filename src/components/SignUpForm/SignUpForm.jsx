150dd01bdc3a3b3b3d4cd6156e5d38e4af27b617
ism90 <ianmason5@gmail.com>
2021-10-18 16:17:54 +0100

    page styled and routing for back to login

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,46 +1,44 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import {Link} from "react-router-dom";
 
-import ShowPassword from "../../assets/login/show-password.svg"
-import HidePassword from "../../assets/login/hide-password.svg"
+import ShowPassword from "../../assets/login/show-password.svg";
+import HidePassword from "../../assets/login/hide-password.svg";
+
+import "./SignUpForm.scss";
+
+const SignUpForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="signUp">
+      <form action="submit" className="signUp-form">
+        <div className="signUp-form__rectangle"></div>
+        <h3 className="signUp-form__header">Sign Up</h3>
+        <label htmlFor="text" className="signUp-form__label">
+          First Name
+        </label>
+        <input type="text" id="first-name" className="signUp-form__input" />
+        <label htmlFor="text" className="signUp-form__label">
+          Last Name
+        </label>
+        <input type="text" id="last-name" className="signUp-form__input" />
+        <label htmlFor="email" className="signUp-form__label">
+          Email Address
+        </label>
+        <input type="email" id="email" className="login-form__input" />
 
 
+        <button className="signUp-form__button">Sign Up</button>
+      </form>
 
-import "./SignUpForm.scss"
+      <p className="signUp-login">
+        Already have an account? <Link to = "/login">Login</Link>
+      </p>
+    </div>
+  );
+};
 
-
-const LoginForm = () => {
-    
-    const [showPassword, setShowPassword] = useState(false)
-    const handleShowPassword = () => {
-        setShowPassword(!showPassword)
-    }
-    
-    return (
-        <div className = "login">
-            <form action="submit" className="login-form">
-                <div className="login-form__rectangle"></div>
-                <h3 className="login-form__header">Sign Up</h3>
-                <label htmlFor="email" className="login-form__label">Email Address</label>
-                <input type="text" id="email" className="login-form__input" />
-                <label htmlFor="password" className="login-form__label">Password</label>
-                <div className="login-form__password-container">
-                    <input type={!showPassword ? "password" : "text"} id="password" className="login-form__input" />
-                        {!showPassword && <img src={HidePassword} className="hidepassword" alt="Hide password icon" onClick={handleShowPassword} />}
-                        {showPassword && <img src={ShowPassword} className="showpassword" alt="Show password icon" onClick={handleShowPassword} />}
-                </div>
-                <div className="login-form__checkbox-container">
-                    <input type="checkbox" id="remember-me" className="login-form__checkbox" />
-                    <label htmlFor="remember-me" className="checkbox-label">Remember Me</label>
-                </div>
-                <button className="login-form__button">Login</button>
-            </form>
-                
-            <p className="login-signup">Don't have an account? <span>Sign Up</span></p>
-            <p className="login-link">Forgotten Your Password?</p>
-            
-        </div>
-    )
-}
-
-export default LoginForm
-
+export default SignUpForm;
