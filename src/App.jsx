1c555327228ac619a35bf018760ede37e5179be4
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-10-19 15:26:06 +0100

    removing errors and returning app to clean state

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./containers/Routes/Routes";
-import SecurityRiskBreakdown from "./components/SecurityRiskBreakdown/SecurityRiskBreakdown";
-import { dataset } from "./data/securityRisk";
-import Dashboard from "./containers/Dashboard/Dashboard";
+
 const App = () => {
   return (
     <div className="App">
-      <Dashboard />
-      {/* <Router>
+      <Router>
         <Routes />
-      </Router> */}
+      </Router>
     </div>
   );
 };
