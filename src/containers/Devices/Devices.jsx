358638f4817aa8e0274520b91c033e2fdad94eab
aliabdellatif0 <ali.abdlaitf@gmail.com>
2021-10-19 15:53:32 +0100

    done

--- src/containers/Devices/Devices.jsx
+++ src/containers/Devices/Devices.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import DeviceRisk from "../../components/DeviceRisk/DeviceRisk";
+import DeviceList from "../DeviceList/DeviceList";
 import "./Devices.scss";
 
 const Devices = () => {
   return (
     <div className="devices__text">
-      
-      <DeviceRisk title="hello" message="Risky device" />
+      <DeviceList />
     </div>
   );
 };
