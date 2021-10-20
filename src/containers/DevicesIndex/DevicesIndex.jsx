0d176a4a63b008574fa467471cdb4b314a4a6f78
aliabdellatif0 <ali.abdlaitf@gmail.com>
2021-10-20 15:33:07 +0100

    pop up made now functioning is needed

--- src/containers/DevicesIndex/DevicesIndex.jsx
+++ src/containers/DevicesIndex/DevicesIndex.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router";
 import DeviceInfo from "../../components/DeviceInfo/DeviceInfo";
 import DeviceRisk from "../../components/DeviceRisk/DeviceRisk";
 import { devices } from "../../data/devices";
+import "./DevicesIndex.scss";
 
 const DevicesIndex = () => {
   const { device } = useParams();
+
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
   const filteredDevice = devices.find(
     (singleDevice) => singleDevice.name == device
   );
@@ -14,8 +22,31 @@ const DevicesIndex = () => {
     <div>
       <h3>{device}</h3>
       <h4>{filteredDevice.brand}</h4>
-      <DeviceInfo name = {filteredDevice.name} type = {filteredDevice.type} brand = {filteredDevice.brand} model = {filteredDevice.model} OS = {filteredDevice.OS} ipAddress = {filteredDevice.ipAddress} lastActive = {filteredDevice.lastActive}/>
-      <DeviceRisk securityRisk={filteredDevice.securityRisk}/>
+      <DeviceInfo
+        name={filteredDevice.name}
+        type={filteredDevice.type}
+        brand={filteredDevice.brand}
+        model={filteredDevice.model}
+        OS={filteredDevice.OS}
+        ipAddress={filteredDevice.ipAddress}
+        lastActive={filteredDevice.lastActive}
+      />
+      <DeviceRisk securityRisk={filteredDevice.securityRisk} />
+
+      <div className="modal">
+        <div className="overlay"> </div>
+        <div className="modal-content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+            vero expedita, debitis rem quos minus eaque cum fugit impedit rerum,
+            aspernatur quam facere quasi nihil reprehenderit perferendis
+            molestiae officiis distinctio.
+          </p>
+          <button className="close-modal" onClick={toggleModal}>
+            Close
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
