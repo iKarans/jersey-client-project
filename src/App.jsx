064358cc525e204b69c0c4d13f42b98ff7836e98
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-10-20 14:57:01 +0100

    responsive changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./containers/Routes/Routes";
+
 const App = () => {
   return (
     <div className="App">
