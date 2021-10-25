46dc5c58430d35621c10b77adac9c1178e5c6f78
matthew hutt <matt.hutt85@gmail.com>
2021-10-25 16:33:39 +0100

    initial design and content finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Security from "./containers/Security/Security";
+
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./containers/Routes/Routes";
@@ -7,10 +9,13 @@ import Routes from "./containers/Routes/Routes";
 const App = () => {
   return (
     <div className="App">
+      <Security />
+      
       <Router>
         <Routes />
       </Router>
     </div>
+    
   );
 };
 export default App;
--- src/containers/Security/Security.jsx
+++ src/containers/Security/Security.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./Security.scss"
 import SecurityScore from '../../components/SecurityScore/SecurityScore'
+import CalulationSummary from '../../components/CalculationSummary/CalulationSummary'
 
 const Security = () => {
   return (
     <div className="security">
       <SecurityScore />
+      <CalulationSummary />
     </div>
   )
 }
