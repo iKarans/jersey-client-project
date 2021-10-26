02f794d4e3c804b9ea6567088e22c6d85d7ae769
itsyas <itsyas@users.noreply.github.com>
2021-10-26 15:30:36 +0100

    sass nesting

--- src/components/CalculationSummary/CalulationSummary.jsx
+++ src/components/CalculationSummary/CalulationSummary.jsx
@@ -5,21 +5,21 @@ import Cross from "../../assets/login/red-cross.svg";
 
 const CalulationSummary = () => {
     return (
-        <div className="container">
-            <div className="calculation">
-                <h1 className="calculation__title">Calculation Summary</h1>
-                <p className="calculation__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sequi laboriosam asperiores repellat quas dolores excepturi.</p>
+        <div className="calculation">
+            <div className="calculation__container">
+                <h1 className="calculation__container--header">Calculation Summary</h1>
+                <p className="calculation__container--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sequi laboriosam asperiores repellat quas dolores excepturi.</p>
 
-                <div className="calculation__container">
-                    <div className="calculation__container--items">
-                        <img className="calculation__container--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__container--paragraph">Hard drives encrypted</p>
-                        <img className="calculation__container--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__container--paragraph">Anti-virus and malware installed</p>
-                        <img className="calculation__container--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__container--paragraph">No high risk devices detected on network</p>
-                        <img className="calculation__container--image" src={Cross} alt="Red-Cross"/>
-                        <p className="calculation__container--paragraph" style={{color: '#F08E8E'}}>No SSL Certificates</p>
+                <div className="calculation__box">
+                    <div className="calculation__items">
+                        <img className="calculation__items--image" src={Tick} alt="Green tick"/>
+                        <p className="calculation__items--text">Hard drives encrypted</p>
+                        <img className="calculation__items--image" src={Tick} alt="Green tick"/>
+                        <p className="calculation__items--text">Anti-virus and malware installed</p>
+                        <img className="calculation__items--image" src={Tick} alt="Green tick"/>
+                        <p className="calculation__items--text">No high risk devices detected on network</p>
+                        <img className="calculation__items--image" src={Cross} alt="Red-Cross"/>
+                        <p className="calculation__items--text" style={{color: '#F08E8E'}}>No SSL Certificates</p>
                     </div>
                 </div>
 
--- src/containers/Security/Security.jsx
+++ src/containers/Security/Security.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./Security.scss"
-import SecurityScore from '../../components/SecurityScore/SecurityScore'
+// import SecurityScore from '../../components/SecurityScore/SecurityScore'
 import CalulationSummary from '../../components/CalculationSummary/CalulationSummary'
 
 const Security = () => {
   return (
     <div className="security">
-      <SecurityScore />
+      {/* <SecurityScore /> */}
       <CalulationSummary />
     </div>
   )
