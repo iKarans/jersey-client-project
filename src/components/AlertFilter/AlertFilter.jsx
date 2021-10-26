2b296300c89936d88fbe44351b1e495f59e65b83
iKarans <68602829+iKarans@users.noreply.github.com>
2021-10-26 16:54:30 +0100

    changed networkfilter to altertfilter

--- src/components/AlertFilter/AlertFilter.jsx
+++ src/components/AlertFilter/AlertFilter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./alertFilter.scss";
 
 const AlertFilter = () => {
-
+  const {handleFilterCheckbox, handleIsLowImportance, handleIsHighImportance, handleIsLessHour, handleIsLessThreeHour} = props
 
   return (
     
@@ -12,31 +12,31 @@ const AlertFilter = () => {
           <div className="alert-filters__filter--alert" >
             <p className="alert-filters__filter-text">Alert Type</p>
             <form action="" className="alert-filters__filter--alert-form" />
-              <input type="checkbox" />
-              <label htmlFor="" className="alert-filters__checkbox-text">New Device</label><br/>
-              <input type="checkbox" />
-              <label htmlFor="" className="alert-filters__checkbox-text">Outage</label><br/>
-              <input type="checkbox" />
-              <label htmlFor="" className="alert-filters__checkbox-text">High Device Risk</label><br/>
-              <input type="checkbox" />
-              <label htmlFor="" className="alert-filters__checkbox-text">Device Logon</label>
+              <input type="checkbox" onInput={handleFilterCheckbox} id="New Device"/>
+              <label htmlFor="New Device" className="alert-filters__checkbox-text">New Device</label><br/>
+              <input type="checkbox" onInput={handleFilterCheckbox} id="Outage"/>
+              <label htmlFor="Outage" className="alert-filters__checkbox-text">Outage</label><br/>
+              <input type="checkbox" onInput={handleFilterCheckbox} id="High risk"/>
+              <label htmlFor="High risk" className="alert-filters__checkbox-text">High Device Risk</label><br/>
+              <input type="checkbox" onInput={handleFilterCheckbox} id="Logged On" />
+              <label htmlFor="Logged On" className="alert-filters__checkbox-text">Device Logon</label>
             <form/>
           </div>
           <div className="alert-filters__filter--created">
             <p className="alert-filters__filter-text">Created</p>
             <form action="" className="alert-filters__created--filters" />
-              <input type="checkbox" />
+              <input type="radio" name="time" />
               <label htmlFor="" className="alert-filters__checkbox-text">Less than 1hr ago</label><br/>
-              <input type="checkbox"  />
+              <input type="radio" name="time"/>
               <label htmlFor="" className="alert-filters__checkbox-text">1 - 3hrs ago</label>
             <form/>
           </div>
           <div className="alert-filters__filter--importance">
             <p className="alert-filters__filter-text">Importance Level</p>
             <form action="" className="alert-filters__importance--filters" />
-              <input type="checkbox" />
+              <input type="radio" name="importance"/>
               <label htmlFor="" className="alert-filters__checkbox-text">Low</label><br/>
-              <input type="checkbox"  />
+              <input type="radio" name="importance" />
               <label htmlFor="" className="alert-filters__checkbox-text">High</label>
             <form/>
           </div>
