1d4558afaa86e2159963fddcd29098cc5409d787
hpteach2021 <hooper.rob@gmail.com>
2021-10-26 16:39:38 +0100

    changes for karan

---
+++ src/containers/Alerts/AlertFunctions.js
@@ -0,0 +1,106 @@
+  export let checkListArray = [];
+  export let isDeviceChecked = false;
+  export let isOutageChecked = false;
+  export let isHighRiskChecked = false;
+  export let isLoggedOnChecked = false;
+  export let isLessThanHourChecked = false;
+  export let isOneToThreeHoursChecked = false;
+  export let isLowImportanceChecked = false;
+  export let isHighImportanceChecked = false;
+
+
+
+  export const handleChangeDevice = () => {
+
+    if (checkListArray.includes("New Device")) {
+      checkListArray = checkListArray.filter(item => item !== "New Device")
+      let isDeviceChecked = false;
+      console.log(checkListArray)
+      console.log(isDeviceChecked)
+    } else {
+      checkListArray.push("NewDevice");
+      let isDeviceChecked = true;
+      console.log(checkListArray)
+      console.log(isDeviceChecked)
+    }
+
+  };
+  export const handleChangeOutage = () => {
+    if (checkListArray.includes("Outage")) {
+      checkListArray = checkListArray.filter(item => item !== "Outage")
+      let isOutageChecked = false;
+      console.log(checkListArray)
+    } else {
+      checkListArray.push("Outage");
+      let isOutageChecked = true;
+      console.log(checkListArray)
+    }
+  };
+  export const handleChangeHighRisk = () => {
+    if (checkListArray.includes("High Risk")) {
+      let isHighRiskChecked = false;
+      checkListArray = checkListArray.filter(item => item !== "High Risk")
+      console.log(checkListArray)
+    } else {
+      checkListArray.push("High Risk");
+      let isHighRiskChecked = true;
+      console.log(checkListArray)
+    }
+  };
+  export const handleChangeLoggedOn = () => {
+    if (checkListArray.includes("Logged On")) {
+      let isLoggedOnChecked = false;
+      checkListArray = checkListArray.filter(item => item !== "Logged On")
+      console.log(checkListArray)
+    } else {
+      checkListArray.push("Logged On");
+      let isLoggedOnChecked = true;
+      console.log(checkListArray)
+    }
+  };
+
+  // export const handleChangeLessThanHour = () => {
+  //   if (checkListArray.includes("hour")) {
+  //     let isLessThanHourChecked = false;
+  //     checkListArray = checkListArray.filter(item => item !== "hour")
+  //     console.log(checkListArray)
+  //   } else {
+  //     checkListArray.push("hour");
+  //     let isLessThanHourChecked = true;
+  //     console.log(checkListArray)
+  //   }
+  // };
+  // export const handleChangeOneToThreeHours = () => {
+  //   if (checkListArray.includes("oneToThreeHours")) {
+  //     let isOneToThreeHoursChecked = false;
+  //     checkListArray = checkListArray.filter(item => item !== "oneToThreeHours")
+  //     console.log(checkListArray)
+  //   } else {
+  //     checkListArray.push("oneToThreeHours");
+  //     let isOneToThreeHoursChecked = true;
+  //     console.log(checkListArray)
+  //   }
+  // };
+
+  // export const handleChangeLowImportance = () => {
+  //   if (checkListArray.includes("lowImportance")) {
+  //     let isLowImportanceChecked = false;
+  //     checkListArray = checkListArray.filter(item => item !== "lowImportance")
+  //     console.log(checkListArray)
+  //   } else {
+  //     checkListArray.push("lowImportance");
+  //     let isLowImportanceChecked = true;
+  //     console.log(checkListArray)
+  //   }
+  // };
+  // export const handleChangeHighImportance = () => {
+  //   if (checkListArray.includes("highImportance")) {
+  //     let isHighImportanceChecked = false;
+  //     checkListArray = checkListArray.filter(item => item !== "highImportance")
+  //     console.log(checkListArray)
+  //   } else {
+  //     checkListArray.push("highImportance");
+  //     let isHighImportanceChecked = true;
+  //     console.log(checkListArray)
+  //   }
+  // };
--- src/containers/Alerts/Alerts.jsx
+++ src/containers/Alerts/Alerts.jsx
@@ -1,30 +1,21 @@
-import React, { useState, useEffect } from "react";
-// import { isDeviceChecked } from "./NetworkFunctions";
-import NetworkItem from "../../components/NetworkItem/NetworkItem";
+import React, { useState } from "react";
+import { isDeviceChecked } from "./AlertFunctions";
+import AlertItem from "../../components/AlertItem/AlertItem";
 import ChatButton from "../../components/ChatButton/ChatButton";
-import NetworkPageButton from "../../components/NetworkPageButton/NetworkPageButton";
-import { networks } from "../../data/network.js";
-import {
-  handleChangeDevice,
-  handleChangeHighRisk,
-  handleChangeLoggedOn,
-  handleChangeOutage,
-  checkListArray,
-} from "./NetworkFunctions";
+import AlertPageButton from "../../components/AlertPageButton/AlertPageButton";
+import { alerts } from "../../data/alerts.js";
 
 import SearchIcon from "../../assets/global/search-icon.svg";
 import SortIcon from "../../assets/global/sort-icon.svg";
 import FilterIcon from "../../assets/devices/filterIcon.png";
 import WhiteDropDown from "../../assets/global/white-dropdown.svg";
-import NetworkArrow from "../../assets/network/network-arrow.svg";
+import AlertsArrow from "../../assets/alerts/alerts-arrow.svg";
 
-import "./Network.scss";
-import NetworkFilter from "../../components/NetworkFilter/NetworkFilter";
+import "./Alerts.scss";
 
-const Network = () => {
+const Alerts = () => {
   const [pages, setPages] = useState(0);
-  const [splitNetworkArrays] = useState([]);
-  const [filterStatus, setFilterStatus] = useState(false);
+  const [splitAlertsArrays] = useState([]);
 
   const handleDecrement = () => {
     if (pages > 0) {
@@ -33,77 +24,25 @@ const Network = () => {
   };
 
   const handleIncrement = () => {
-    if (pages >= 0 && pages < networks.length / 9 - 1) {
+    if (pages >= 0 && pages < alerts.length / 9 - 1) {
       setPages(pages + 1);
     }
   };
-  const networksArray = [...networks];
 
-    /// Karans Messing around
-    const [filtersArray, setFiltersArray] = useState([])
-    const handleFilterCheckbox = (event) => {
-      let tempArr = [...filtersArray];
-      if(tempArr.includes(event.target.id)) {
-        tempArr.splice(tempArr.indexOf(event.target.id), 1);
-      } else {
-        tempArr.push(event.target.id);
-      }
-      setFiltersArray(tempArr);
-    }
-    // importance Level
-    const [isLowImportance, setLowImportanceLevel] = useState(false);
-    const [isHighImportance, setHighImportanceLevel] = useState(false);
-    const handleIsLowImportance = () => {
-      setLowImportanceLevel(!isLowImportance);
-    }
-    const handleIsHighImportance = () => {
-      setHighImportanceLevel(!isHighImportance);
-    }
+  const alertsArray = [...alerts];
 
-    const [isLessHour, setIsLessHour] = useState(false);
-    const [isLessThreeHour, setIsLessThreeHour] = useState(false);
-    const handleIsLessHour = () => {
-      setIsLessHour(!isLessHour);
-    }
-    const handleIsLessThreeHour = () => {
-      setIsLessThreeHour(!isLessThreeHour);
-    }
-
-  const shouldReturn = (network) => {
-    // currentFilters.forEach(filter => {
-    //   if(network.alertType.includes(filter)) {
-    //     console.log("check");
-    //     return true;
-    //   }
-    // })
-    for (let i = 0; i < filtersArray.length; i++) {
-      if (network.alertType.includes(filtersArray[i])) {
-        return true;
-      }
-    }
-    return false;
-  };
-
-  const networksArrayFilteredBySubHour = networksArray
-    .filter((network) => {
-      return shouldReturn(network) && (isLowImportance ? network.importanceID == 1 : true) && (isHighImportance ? network.importanceID == 2 : true) && (isLessHour ? network.createdTime <= 60 : true) && (isLessThreeHour ? network.createdTime > 60 : true)
-    })
-    .sort((a, b) => a.createdTime - b.createdTime);
-  
-    console.log(networksArrayFilteredBySubHour);
-
-  while (networksArray.length > 0) {
-    splitNetworkArrays.push(networksArray.splice(0, 9));
+  while (alertsArray.length > 0) {
+    splitAlertsArrays.push(alertsArray.splice(0, 9));
   }
 
-  const networkItemJSX = splitNetworkArrays[pages].map((network, index) => {
+  const alertsItemJSX = splitAlertsArrays[pages].map((alert, index) => {
     return (
-      <NetworkItem
-        key={network + index}
-        name={network.alertType}
-        summary={network.summary}
-        created={network.created}
-        importanceLevel={network.importanceLevel}
+      <AlertItem
+        key={alert + index}
+        name={alert.alertType}
+        summary={alert.summary}
+        created={alert.created}
+        importanceLevel={alert.importanceLevel}
         index={index}
       />
     );
@@ -112,19 +51,16 @@ const Network = () => {
   const buttonNumbers = [];
 
   const generateButtonIndex = () => {
-    for (let i = 0; i < networks.length / 9; i++) {
+    for (let i = 0; i < alerts.length / 9; i++) {
       buttonNumbers.push(i);
     }
-  };
-  const toggleFilterBox = () => {
-    setFilterStatus(!filterStatus);
   };
 
   generateButtonIndex();
 
   const buttonJSX = buttonNumbers.map((buttonNumber, index) => {
     return (
-      <NetworkPageButton
+      <AlertPageButton
         key={buttonNumber + index}
         index={index + 1}
         setPages={setPages}
@@ -135,101 +71,82 @@ const Network = () => {
 
   return (
     <>
-      <section className="network">
-        <NetworkFilter
-          // onclick={toggleFilterBox}
-          // handleChangeDevice={handleChangeDevice}
-          // handleChangeHighRisk={handleChangeHighRisk}
-          // handleChangeLoggedOn={handleChangeLoggedOn}
-          // handleChangeOutage={handleChangeOutage}
-          handleFilterCheckbox={handleFilterCheckbox}
-          handleIsLowImportance={handleIsLowImportance}
-          handleIsHighImportance={handleIsHighImportance}
-          handleIsLessHour={handleIsLessHour}
-          handleIsLessThreeHour={handleIsLessThreeHour}
-        />
-        <div className="network__search">
-          <div className="network__search-box">
-            <img
-              className="network__icons"
-              src={SearchIcon}
-              alt="search-icon"
-            />
+      <section className="alerts">
+        <div className="alerts__search">
+          <div className="alerts__search-box">
+            <img className="alerts__icons" src={SearchIcon} alt="search-icon" />
             <input
               type="text"
               placeholder="Search Alerts"
               className="search-input"
             />
           </div>
-          <div className="network__search-filter-sort">
-            <div className="network-sort">
-              <img className="network__icons" src={SortIcon} alt="sort-icon" />
-              <p className="network__text-paragraph">Sort</p>
+          <div className="alerts__search-filter-sort">
+            <div className="alerts-sort">
+              <img className="alerts__icons" src={SortIcon} alt="sort-icon" />
+              <p className="alerts__text-paragraph">Sort</p>
             </div>
-            <div className="network-filter">
+            <div className="alerts-filter">
               <img
-                className="network__icons"
+                className="alerts__icons"
                 src={FilterIcon}
                 alt="filter-icon"
               />
-              <p className="network__text-paragraph" onClick={toggleFilterBox}>
-                Filter
-              </p>
+              <p className="alerts__text-paragraph">Filter</p>
             </div>
           </div>
         </div>
-        <div className="network-alerts">
-          <div className="network-alerts__header">
-            <div className="network-alerts__header-alert">
+        <div className="alerts-table">
+          <div className="alerts-table__header">
+            <div className="alerts-table__header-alert">
               <h5 className="alert-header">Alert Type</h5>
               <img src={WhiteDropDown} alt="dropdown" />
             </div>
-            <div className="network-alerts__header-summary">
+            <div className="alerts-table__header-summary">
               <h5 className="alert-header">Sumary</h5>
             </div>
-            <div className="network-alerts__header-created">
+            <div className="alerts-table__header-created">
               <h5 className="alert-header">Created</h5>
               <img src={WhiteDropDown} alt="dropdown" />
             </div>
-            <div className="network-alerts__header-importance">
+            <div className="alerts-table__header-importance">
               <h5 className="alert-header">Importance Level</h5>
               <img src={WhiteDropDown} alt="dropdown" />
             </div>
           </div>
-          <div className="network-alerts__alerts">{networkItemJSX}</div>
-          <div className="network-alerts__pages">
-            {networks.length > 9 && (
-              <div className="network-alerts__pages-buttons">
+          <div className="alerts-table__alerts">{alertsItemJSX}</div>
+          <div className="alerts-table__pages">
+            {alerts.length > 9 && (
+              <div className="alerts-table__pages-buttons">
                 <button
-                  className="network-alerts__pages-buttons-button"
+                  className="alerts-table__pages-buttons-button"
                   onClick={handleDecrement}
                 >
                   <img
-                    src={NetworkArrow}
+                    src={AlertsArrow}
                     alt="left-arrow"
-                    className="network__left-arrow"
+                    className="alerts__left-arrow"
                   />
                 </button>
                 {buttonJSX}
                 <button
-                  className="network-alerts__pages-buttons-button"
+                  className="alerts-table__pages-buttons-button"
                   onClick={handleIncrement}
                 >
                   <img
-                    src={NetworkArrow}
+                    src={AlertsArrow}
                     alt="right-arrow"
-                    className="network__right-arrow"
+                    className="alerts__right-arrow"
                   />
                 </button>
               </div>
             )}
           </div>
         </div>
-
         <ChatButton />
       </section>
     </>
   );
 };
 
-export default Network;
+export default Alerts;
