df8935c2f47439539d6da280b3e78494acca8bd1
itsyas <itsyas@users.noreply.github.com>
2021-10-26 15:09:05 +0100

    resolved issue

--- src/components/CalculationSummary/CalulationSummary.jsx
+++ src/components/CalculationSummary/CalulationSummary.jsx
@@ -7,19 +7,19 @@ const CalulationSummary = () => {
     return (
         <div className="container">
             <div className="calculation">
-                <h1 className="calculation__header">Calculation Summary</h1>
-                <p className="calculation__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sequi laboriosam asperiores repellat quas dolores excepturi.</p>
+                <h1 className="calculation__title">Calculation Summary</h1>
+                <p className="calculation__paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae sequi laboriosam asperiores repellat quas dolores excepturi.</p>
 
                 <div className="calculation__container">
-                    <div className="calculation__container-items">
+                    <div className="calculation__container--items">
                         <img className="calculation__container--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__container--text">Hard drives encrypted</p>
+                        <p className="calculation__container--paragraph">Hard drives encrypted</p>
                         <img className="calculation__container--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__container--text">Anti-virus and malware installed</p>
+                        <p className="calculation__container--paragraph">Anti-virus and malware installed</p>
                         <img className="calculation__container--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__container--text">No high risk devices detected on network</p>
+                        <p className="calculation__container--paragraph">No high risk devices detected on network</p>
                         <img className="calculation__container--image" src={Cross} alt="Red-Cross"/>
-                        <p className="calculation__container--text" style={{color: '#F08E8E'}}>No SSL Certificates</p>
+                        <p className="calculation__container--paragraph" style={{color: '#F08E8E'}}>No SSL Certificates</p>
                     </div>
                 </div>
 
