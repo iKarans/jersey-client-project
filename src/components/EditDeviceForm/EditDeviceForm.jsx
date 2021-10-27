bc130c31fa73527e5547e9a6629a9bb7b00a498c
gabagardi <g.agardi88@gmail.com>
2021-10-27 15:28:32 +0100

    created form and scss for it

---
+++ src/components/EditDeviceForm/EditDeviceForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import "./EditDeviceForm.scss"
+
+const EditDeviceForm = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default EditDeviceForm
