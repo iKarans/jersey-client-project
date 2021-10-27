950b511e493aacf6c45779593c106488fea59ec8
matthew hutt <matt.hutt85@gmail.com>
2021-10-27 15:33:55 +0100

    final stylings finished

--- src/components/CalculationSummary/CalulationSummary.jsx
+++ src/components/CalculationSummary/CalulationSummary.jsx
@@ -19,14 +19,17 @@ const CalulationSummary = (props) => {
 
                 <div className="calculation__box">
                     <div className="calculation__items">
+                        
                         <img className="calculation__items--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__items--text">Hard drives encrypted</p>
+                        <p className="calculation__items--text">Boundary Firewalls</p>
                         <img className="calculation__items--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__items--text">Anti-virus and malware installed</p>
-                        <img className="calculation__items--image" src={Tick} alt="Green tick"/>
-                        <p className="calculation__items--text">No high risk devices detected on network</p>
+                        <p className="calculation__items--text">Anti-Virus</p>
                         <img className="calculation__items--image" src={Cross} alt="Red-Cross"/>
-                        <p className="calculation__items--text" style={{color: '#F08E8E'}}>No SSL Certificates</p>
+                        <p className="calculation__items--text" style={{color: '#F08E8E'}}>Software Updates</p>
+                        <img className="calculation__items--image" src={Tick} alt="Green tick"/>
+                        <p className="calculation__items--text">Secure Configuration</p>
+                        <img className="calculation__items--image" src={Cross} alt="Red-Cross"/>
+                        <p className="calculation__items--text" style={{color: '#F08E8E'}}>Access Control</p>
                     </div>
                 </div>
 
