4ba3516c7fc711038828ef1109e0368abc08e1a3
iagardi <garbigz@gmail.com>
2021-10-28 16:14:23 +0100

    fix

--- src/components/EditDeviceForm/EditDeviceForm.jsx
+++ src/components/EditDeviceForm/EditDeviceForm.jsx
@@ -9,18 +9,36 @@ const EditDeviceForm = () => {
     const [deviceBrand, setDeviceBrand] = useState("Apple")
     const [deviceOS, setDeviceOS] = useState("Mac OS Catalina")
 
+    const [showDeviceTypes, setShowDeviceTypes] = useState(false)
+    const [showDeviceBrands, setShowDeviceBrands] = useState(false)
+    const [showDeviceOS, setShowDeviceOS] = useState(false)
 
 
     const handleDeviceType = (event) => {
         setDeviceType(event.target.textContent)
+        setShowDeviceTypes(!showDeviceTypes)
+    }
+
+    const showDeviceTypesJSX = () => {
+        setShowDeviceTypes(!showDeviceTypes)
     }
 
     const handleDeviceBrand = (event) => {
         setDeviceBrand(event.target.textContent)
+        setShowDeviceBrands(!showDeviceBrands)
+    }
+
+    const showDeviceBrandsJSX = (event) => {
+        setShowDeviceBrands(!showDeviceBrands)
     }
 
     const handleDeviceOS = (event) => {
         setDeviceOS(event.target.textContent)
+        setShowDeviceOS(!showDeviceOS)
+    }
+
+    const showDeviceOSJSX = (event) => {
+        setShowDeviceOS(!showDeviceOS)
     }
 
 
@@ -31,39 +49,41 @@ const EditDeviceForm = () => {
             <h2 className="edit-device__header">Edit Device Details</h2>
             <label htmlFor="device-type" className="edit-device__label">Device Type</label>
             <div className="edit-device__select">
-                <div className="edit-device__select-current" value="desktop">{deviceType} <img src={DownArrow} alt="down arrow" className="edit-device__select-current-arrow" /></div>
-                <div className="edit-device__select-dropdown">
-                    <div className="edit-device__select-dropdown-content" onClick={handleDeviceType}>Laptop</div>
-                    <div className="edit-device__select-dropdown-content" onClick={handleDeviceType}>Tablet</div>
-                    <div className="edit-device__select-dropdown-content" onClick={handleDeviceType}>Mobile</div>
-                    <div className="edit-device__select-dropdown-content" onClick={handleDeviceType}>Desktop</div>
-                </div>
+                <div className="edit-device__select-current" onClick={showDeviceTypesJSX} >{deviceType} <img src={DownArrow} alt="down arrow" className="edit-device__select-current-arrow" /></div>
+                {showDeviceTypes &&
+                    <div className="edit-device__select-dropdown">
+                        <div className="edit-device__select-dropdown-content" onClick={handleDeviceType}>Laptop</div>
+                        <div className="edit-device__select-dropdown-content" onClick={handleDeviceType}>Tablet</div>
+                        <div className="edit-device__select-dropdown-content" onClick={handleDeviceType}>Mobile</div>
+                        <div className="edit-device__select-dropdown-content" onClick={handleDeviceType}>Desktop</div>
+                    </div>
+                }
             </div>
             <label htmlFor="device-name" className="edit-device__label">Device Name</label>
             <input type="text" name="device-name" className="edit-device__input" />
             <label htmlFor="brand" className="edit-device__label">Brand</label>
             <div className="edit-device__select">
-                <div className="edit-device__select-current" value="desktop">{deviceBrand} <img src={DownArrow} alt="down arrow" className="edit-device__select-current-arrow" /></div>
-                <div className="edit-device__select-dropdown">
+                <div className="edit-device__select-current" onClick={showDeviceBrandsJSX}>{deviceBrand} <img src={DownArrow} alt="down arrow" className="edit-device__select-current-arrow" /></div>
+                {showDeviceBrands && <div className="edit-device__select-dropdown">
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceBrand}>Apple</div>
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceBrand}>Huawei</div>
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceBrand}>Lenovo</div>
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceBrand}>Dell</div>
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceBrand}>Samsung</div>
-                </div>
+                </div>}
             </div>
             <label htmlFor="model" className="edit-device__label">Model</label>
             <input type="text" name="model" className="edit-device__input" />
             <label htmlFor="operating-system" className="edit-device__label">Operating System</label>
             <div className="edit-device__select">
-                <div className="edit-device__select-current" value="desktop">{deviceOS}<img src={DownArrow} alt="down arrow" className="edit-device__select-current-arrow" /></div>
-                <div className="edit-device__select-dropdown">
+                <div className="edit-device__select-current" onClick={showDeviceOSJSX}>{deviceOS}<img src={DownArrow} alt="down arrow" className="edit-device__select-current-arrow" /></div>
+                {showDeviceOS && <div className="edit-device__select-dropdown">
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceOS}>Mac OS</div>
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceOS}>Windows 10</div>
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceOS}>Windows 11</div>
                     <div className="edit-device__select-dropdown-content" onClick={handleDeviceOS}>Android 10</div>
 
-                </div>
+                </div>}
             </div>
             <label htmlFor="device-grouping" className="edit-device__label">Device Grouping</label>
             <input type="text" name="device-grouping" className="edit-device__input" />
