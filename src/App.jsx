65fd708fae17a905161c88179368fe0fafd4e35e
Charlie Richardson <55165280+Charlie-robin@users.noreply.github.com>
2021-11-01 16:53:07 +0000

    Revert "Feature 43 security calculation"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./containers/Routes/Routes";
@@ -8,14 +7,13 @@ import UserProvider from "./context/UserContext/UserContext";
 
 const App = () => {
   return (
-    <div className="App">   
+    <div className="App">
       <Router>
         <UserProvider>
           <Routes />
         </UserProvider>
       </Router>
     </div>
-    
   );
 };
 export default App;
