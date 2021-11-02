51ba2ecb5ec86661202dffa6b827d7c7b16ecf51
ism90 <ianmason5@gmail.com>
2021-11-02 15:19:37 +0000

    merge

--- src/components/DeviceMap/DeviceMap.jsx
+++ src/components/DeviceMap/DeviceMap.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+
 
 const DeviceMap = () => {
   return (
     <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer className = "map-container" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
