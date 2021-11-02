4aab59260f53be4d8c3ed26f219929e9380840fa
alessandrothedev <spiridigliozzi.alessandro@gmail.com>
2021-11-02 15:17:15 +0000

    fixed alert buttons bug

--- src/components/SecurityBar/AlertButton/AlertButton.jsx
+++ src/components/SecurityBar/AlertButton/AlertButton.jsx
@@ -4,20 +4,20 @@ import "./AlertButton.scss";
 const AlertButton = (props) => {
   const { isHigh, buttonText, iconImage, buttonNumber } = props;
 
-  let buttonStyle = "button__icon";
+  let buttonStyle = "alert-button__icon";
 
   isHigh ? (buttonStyle += "--high") : (buttonStyle += "--primary");
 
   return (
-    <button className="button">
-      <div className="button-icon-container">
+    <button className="alert-button">
+      <div className="alert-button-icon-container">
         <div className={buttonStyle}>
-            <img className="button__icon--svg" src={iconImage} alt="alerts" />
+            <img className="alert-button__icon--svg" src={iconImage} alt="alerts" />
         </div>
       </div>
-     <div className="button__info">
-        <p className="button__info--heading">{buttonText}</p>
-        <p className="button__info--number">{buttonNumber}</p>
+     <div className="alert-button__info">
+        <p className="alert-button__info--heading">{buttonText}</p>
+        <p className="alert-button__info--number">{buttonNumber}</p>
      </div>
     </button>
   );
