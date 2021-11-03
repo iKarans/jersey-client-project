c0ef9ac406934c7886fa13ac40b9fbaeb326db42
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-11-03 16:36:02 +0000

    added email submit page

---
+++ src/components/EmailSubmit/EmailSubmit.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+return (
+  <div classname="successful-submit">
+    <div classname="successful-submit__box">
+      <h3 classname="successful-submit__header">Successfully sumbitted!</h3>
+      <p classname="successful-submit__text">
+        You have successfully sumbitted your email, please check for an email
+        and follow the link to complete the reset
+      </p>
+      <p className="successful-submit__text">
+        Back to <Link to="/login">Login</Link>
+      </p>
+    </div>
+  </div>
+);
