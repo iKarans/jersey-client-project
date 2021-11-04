2f21e6aaec84aa9e5bd811e1291744d5fb808cd5
ism90 <ianmason5@gmail.com>
2021-11-04 14:14:40 +0000

    push up if statemenet

--- src/components/DeviceMap/DeviceMap.jsx
+++ src/components/DeviceMap/DeviceMap.jsx
@@ -42,45 +42,48 @@ const DeviceMap = (props) => {
 
   return (
     <>
-    {deviceLatitude && deviceLongitude ? 
-    <div className="map-card">
-    <h4 className="map-card__title">Last Known Location</h4>
+      {(deviceLatitude == "Not Found" && deviceLatitude) ||
+      (deviceLongitude == "Not Found" && deviceLongitude) ? (
+        <div className="map-card">
+          <h4 className="map-card__title">Last Known Location</h4>
 
-    <div>
-      <MapContainer
-        center={mapPosition}
-        zoom={13}
-        scrollWheelZoom={true}
-        className="map-card__map"
-        key={mapPosition}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={mapPosition} className="map-card__pin">
-          <Popup></Popup>
-        </Marker>
-      </MapContainer>
-    </div>
-    <div className="map-card__details">
-      <div className="map-card__longitude">
-        <p className="map-card__details-title">Longitude</p>
-        <p className="map-card__details-content">{deviceLongitude}</p>
-      </div>
-      <div className="map-card__latitude">
-        <p className="map-card__details-title">Latitude</p>
-        <p className="map-card__details-content">{deviceLatitude}</p>
-      </div>
-      <div className="map-card__location">
-        <p className="map-card__details-title">Location</p>
-        <p className="map-card__details-content">
-          {`${cityDetails} ${countryDetails}`}
-        </p>
-      </div>
-    </div>
-  </div>: ""
-    }
+          <div>
+            <MapContainer
+              center={mapPosition}
+              zoom={13}
+              scrollWheelZoom={true}
+              className="map-card__map"
+              key={mapPosition}
+            >
+              <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={mapPosition} className="map-card__pin">
+                <Popup></Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+          <div className="map-card__details">
+            <div className="map-card__longitude">
+              <p className="map-card__details-title">Longitude</p>
+              <p className="map-card__details-content">{deviceLongitude}</p>
+            </div>
+            <div className="map-card__latitude">
+              <p className="map-card__details-title">Latitude</p>
+              <p className="map-card__details-content">{deviceLatitude}</p>
+            </div>
+            <div className="map-card__location">
+              <p className="map-card__details-title">Location</p>
+              <p className="map-card__details-content">
+                {`${cityDetails} ${countryDetails}`}
+              </p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 };
