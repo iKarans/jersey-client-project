30b3e93614b0dfdd6cb3e14485da8a09ce849887
Morris Willis <89452634+Halfiies@users.noreply.github.com>
2021-11-04 15:50:20 +0000

    tidying up

--- src/components/ForgottenPasswordForm/ForgottenPaswordForm.jsx
+++ src/components/ForgottenPasswordForm/ForgottenPaswordForm.jsx
@@ -1,16 +1,11 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase";
-import { sendSignInLinkToEmail, sendPasswordResetEmail } from "firebase/auth";
+import { sendPasswordResetEmail } from "firebase/auth";
 import "./ForgottenPasswordForm.scss";
 
 const ForgottenPasswordForm = () => {
   const [userEmail, setUserEmail] = useState("");
-  const actionCodeSettings = {
-    url: "http://localhost:3000/reset-password",
-
-    handleCodeInApp: true,
-  };
 
   const handleInput = (event) => {
     const emailInput = event.target.value;
