d1e72aff26330bd78e6e60c2f33ed6381a055d2b
itsyas <itsyas@users.noreply.github.com>
2021-11-05 14:29:47 +0000

    resolved changes

--- src/components/ThemeSettings/ThemeSettings.jsx
+++ src/components/ThemeSettings/ThemeSettings.jsx
@@ -9,18 +9,18 @@ const ThemeSettings = () => {
    }
 
    return (
-      <div className="themesettings">
-         <h3 className="themesettings__header">Theme Settings</h3>
-         <p className="themesettings__message">At Lujam, we’re aware that we all have different tastes so please feel free to customise the portal using some of our themes.</p>
-         <form action="" className="themesettings__form">
-            <h4 className="themesettings__form--header">Theme</h4>
-            <div className="themesettings__form--radio">
+      <div className="theme-settings">
+         <h3 className="theme-settings__header">Theme Settings</h3>
+         <p className="theme-settings__message">At Lujam, we’re aware that we all have different tastes so please feel free to customise the portal using some of our themes.</p>
+         <form action="" className="theme-settings__form">
+            <h4 className="theme-settings__form--header">Theme</h4>
+            <div className="theme-settings__form--radio">
                <label for="light">Light</label>
                <input type="radio" id="light" name="fav_language" value="HTML" />
                <label for="dark">Dark</label>
                <input type="radio" id="dark" name="fav_language" value="CSS" />
             </div>
-            <button onClick={handleApply} className="themesettings__form--applybutton">Apply</button>
+            <button onClick={handleApply} className="theme-settings__form--button">Apply</button>
          </form>
       </div>
    )
